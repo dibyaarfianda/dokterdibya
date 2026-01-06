2de5b2bd8a8408743379d239edd95888d6a4b907
Dibya <nanda.arfianda@gmail.com>
2026-01-06 22:11:48 +0700

    chore: Remove test file

--- staff/backend/gen-token.js
+++
@@ -1,3 +0,0 @@
-require('dotenv').config({ path: '/var/www/dokterdibya/staff/backend/.env' });
-const jwt = require('jsonwebtoken');
-console.log(jwt.sign({id:'UDZAQUCQWZ',role:'dokter'},process.env.JWT_SECRET,{expiresIn:'1h'}));
