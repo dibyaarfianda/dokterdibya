770754f30e67ac0f3e5471f9dc3c372ede7c1f7d
Dibya <nanda.arfianda@gmail.com>
2026-01-11 01:45:10 +0700

    fix: Force MainActivity to load web URL directly

--- staff-mobile-app/android/app/src/main/java/com/dokterdibya/staff/MainActivity.java
+++ staff-mobile-app/android/app/src/main/java/com/dokterdibya/staff/MainActivity.java
@@ -1,5 +1,14 @@
 package com.dokterdibya.staff;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Force load the web URL
+        getBridge().getWebView().loadUrl("https://dokterdibya.com/staff/public/index-adminlte.html?mobile=1");
+    }
+}
