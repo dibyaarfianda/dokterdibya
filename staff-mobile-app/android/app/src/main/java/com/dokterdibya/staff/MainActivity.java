6b2b2623ae13db95a6d9589cf9e9a607804d3006
Dibya <nanda.arfianda@gmail.com>
2026-01-11 01:55:12 +0700

    fix: Use pure WebView instead of Capacitor BridgeActivity

--- staff-mobile-app/android/app/src/main/java/com/dokterdibya/staff/MainActivity.java
+++ staff-mobile-app/android/app/src/main/java/com/dokterdibya/staff/MainActivity.java
@@ -1,14 +1,60 @@
 package com.dokterdibya.staff;
 
 import android.os.Bundle;
-import com.getcapacitor.BridgeActivity;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends BridgeActivity {
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private static final String WEB_URL = "https://dokterdibya.com/staff/public/index-adminlte.html?mobile=1";
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Force load the web URL
-        getBridge().getWebView().loadUrl("https://dokterdibya.com/staff/public/index-adminlte.html?mobile=1");
+        // Create WebView programmatically
+        webView = new WebView(this);
+        setContentView(webView);
+
+        // Configure WebView settings
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+        settings.setSupportZoom(false);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+
+        // Handle navigation within WebView
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient());
+
+        // Load the URL
+        webView.loadUrl(WEB_URL);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
