565db6048b08aedcdd03aa8a85a3f82d7f759316
Dibya <nanda.arfianda@gmail.com>
2026-04-12 14:09:34 +0700

    Change surgery badge icon from wrench to scalpel

--- docboard/src/components/CalendarGrid.jsx
+++ docboard/src/components/CalendarGrid.jsx
@@ -47,8 +47,8 @@ export default function CalendarGrid({ year, month, events, surgeryEvents, onDay
               )}
               {hasSurgery && (
                 <div class="calendar-surgery-badge" title={`${surgTotal} tindakan`}>
-                  <svg width="10" height="10" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round">
-                    <path d="M14.7 6.3a1 1 0 000 1.4l1.6 1.6a1 1 0 001.4 0l3.77-3.77a6 6 0 01-7.94 7.94l-6.91 6.91a2.12 2.12 0 01-3-3l6.91-6.91a6 6 0 017.94-7.94l-3.76 3.76z" />
+                  <svg width="9" height="9" viewBox="0 0 24 24" fill="currentColor">
+                    <path d="M7.2 14.4l-2.8 2.8c-1 1-1 2.6 0 3.6s2.6 1 3.6 0l2.8-2.8M16.8 9.6l2.8-2.8c1-1 1-2.6 0-3.6s-2.6-1-3.6 0l-2.8 2.8M8 16l8-8" />
                   </svg>
                   <span>{surgTotal}</span>
                 </div>
