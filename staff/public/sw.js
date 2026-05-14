e80c23eee2a860f8b73545aed793d98ab40e8bb2
Dibya <nanda.arfianda@gmail.com>
2026-05-15 04:55:55 +0700

    Fix chat FAB tidak muncul: waitForAuth baca localStorage langsung, bump v93

--- staff/public/sw.js
+++ staff/public/sw.js
@@ -4,7 +4,7 @@
  * Updated: Real-time friendly for service hours
  */
 
-const STAFF_PWA_VERSION = 'v92';
+const STAFF_PWA_VERSION = 'v93';
 const CACHE_NAME = `dokterdibya-staff-${STAFF_PWA_VERSION}`;
 const STATIC_CACHE = `static-${STAFF_PWA_VERSION}`;
 const DYNAMIC_CACHE = `dynamic-${STAFF_PWA_VERSION}`;
