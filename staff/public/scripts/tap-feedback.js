4f6809724f5c64e964204b66f3bf25d8380483c0
Dibya <nanda.arfianda@gmail.com>
2026-05-15 20:58:34 +0700

    Reduce staff PWA tap click volume by half

--- staff/public/scripts/tap-feedback.js
+++ staff/public/scripts/tap-feedback.js
@@ -42,11 +42,11 @@
             osc.frequency.exponentialRampToValueAtTime(680, ac.currentTime + 0.018);
             osc.frequency.exponentialRampToValueAtTime(420, ac.currentTime + 0.045);
 
-            clickGain.gain.setValueAtTime(0.13, ac.currentTime);
+            clickGain.gain.setValueAtTime(0.065, ac.currentTime);
             clickGain.gain.exponentialRampToValueAtTime(0.0001, ac.currentTime + 0.014);
 
-            bodyGain.gain.setValueAtTime(0.045, ac.currentTime + 0.004);
-            bodyGain.gain.exponentialRampToValueAtTime(0.012, ac.currentTime + 0.028);
+            bodyGain.gain.setValueAtTime(0.0225, ac.currentTime + 0.004);
+            bodyGain.gain.exponentialRampToValueAtTime(0.006, ac.currentTime + 0.028);
             bodyGain.gain.exponentialRampToValueAtTime(0.0001, ac.currentTime + 0.05);
 
             osc.start(ac.currentTime);
