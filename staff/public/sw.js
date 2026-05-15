4a3ddb8b9331d61f02a2e577187a724710cf5687
Dibya <nanda.arfianda@gmail.com>
2026-05-15 20:49:39 +0700

    Add shared mobile tap feedback for staff PWA

--- staff/public/sw.js
+++ staff/public/sw.js
@@ -4,7 +4,7 @@
  * Updated: Real-time friendly for service hours
  */
 
-const STAFF_PWA_VERSION = 'v110';
+const STAFF_PWA_VERSION = 'v111';
 const CACHE_NAME = `dokterdibya-staff-${STAFF_PWA_VERSION}`;
 const STATIC_CACHE = `static-${STAFF_PWA_VERSION}`;
 const DYNAMIC_CACHE = `dynamic-${STAFF_PWA_VERSION}`;
