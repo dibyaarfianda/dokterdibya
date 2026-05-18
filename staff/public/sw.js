d1c3f06ee57dcd45b2ae91ba14933e7053b62b6d
Dibya <nanda.arfianda@gmail.com>
2026-05-18 19:54:18 +0700

    Bump STAFF_PWA_VERSION to v131 to bust SW cache after support chat div ID fix

--- staff/public/sw.js
+++ staff/public/sw.js
@@ -4,7 +4,7 @@
  * Updated: Real-time friendly for service hours
  */
 
-const STAFF_PWA_VERSION = 'v130'; // Support chat staff realtime + Enter send reliability fixes
+const STAFF_PWA_VERSION = 'v131'; // Support chat staff realtime + Enter send reliability fixes
 const CACHE_NAME = `dokterdibya-staff-${STAFF_PWA_VERSION}`;
 const STATIC_CACHE = `static-${STAFF_PWA_VERSION}`;
 const DYNAMIC_CACHE = `dynamic-${STAFF_PWA_VERSION}`;
