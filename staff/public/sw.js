d6185b28c6153daa399aefc84546801ae79854e4
Dibya <nanda.arfianda@gmail.com>
2026-05-25 22:24:19 +0700

    Fix patient activity menu and scrolling

--- staff/public/sw.js
+++ staff/public/sw.js
@@ -4,7 +4,7 @@
  * Updated: Real-time friendly for service hours
  */
 
-const STAFF_PWA_VERSION = 'v217'; // Force refresh USG obstetri multiple fetus format
+const STAFF_PWA_VERSION = 'v218'; // Force refresh patient activity sidebar and scroll fix
 const CACHE_NAME = `dokterdibya-staff-${STAFF_PWA_VERSION}`;
 const STATIC_CACHE = `static-${STAFF_PWA_VERSION}`;
 const DYNAMIC_CACHE = `dynamic-${STAFF_PWA_VERSION}`;
