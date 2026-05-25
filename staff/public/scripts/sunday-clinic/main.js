b47d0b4bdfb4a3f45ecdf720d8d8ee2bebe65340
Dibya <nanda.arfianda@gmail.com>
2026-05-25 20:45:44 +0700

    Add twin format to obstetric USG template

--- staff/public/scripts/sunday-clinic/main.js
+++ staff/public/scripts/sunday-clinic/main.js
@@ -227,7 +227,7 @@ class SundayClinicApp {
     async loadComponents() {
         const componentPaths = this.getComponentPaths();
         // Hard version number - increment this to force reload
-        const COMPONENT_VERSION = '3.0.9';
+        const COMPONENT_VERSION = '3.0.10';
         const cacheBuster = `?v=${COMPONENT_VERSION}`;
 
         const loaders = componentPaths.map(async ({ section, path }) => {
