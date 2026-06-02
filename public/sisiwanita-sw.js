a1901c74ef3045b463853237c7cae8e523367964
Dibya <nanda.arfianda@gmail.com>
2026-06-02 07:33:01 +0700

    Bump sisiwanita SW cache after hiding Obgyn terminology menu

--- public/sisiwanita-sw.js
+++ public/sisiwanita-sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260601fix1';
+const CACHE_VERSION = '20260602hide1';
 const CACHE_PREFIX = 'sisiwanita-patient-portal-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = '/offline.html';
