c8e091140cca9718eaabdd855be02b111e23ccab
Dibya <nanda.arfianda@gmail.com>
2026-06-04 23:51:29 +0700

    Animate SISIwanita login CTA and allow domain CORS

--- public/sisiwanita-sw.js
+++ public/sisiwanita-sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260604login4';
+const CACHE_VERSION = '20260604login5';
 const CACHE_PREFIX = 'sisiwanita-patient-portal-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = '/offline.html';
