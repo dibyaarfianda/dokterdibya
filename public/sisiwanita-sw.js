707069f0f59a3907c5ecce60aca425d30a62a1f3
Dibya <nanda.arfianda@gmail.com>
2026-06-05 20:57:15 +0700

    Switch PWA icons to swlogo2.svg source

--- public/sisiwanita-sw.js
+++ public/sisiwanita-sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260605icon1';
+const CACHE_VERSION = '20260605icon4'; // switch to swlogo2 icons
 const CACHE_PREFIX = 'sisiwanita-patient-portal-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = '/offline.html';
