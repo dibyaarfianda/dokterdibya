fbe31d2d1d43125ae4ad435de38d38b34f715db8
Dibya <nanda.arfianda@gmail.com>
2026-06-05 19:54:31 +0700

    Fix SISIwanita PWA icon crop on Android - separate any/maskable icons

--- public/sisiwanita-sw.js
+++ public/sisiwanita-sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260605birth1';
+const CACHE_VERSION = '20260605icon1';
 const CACHE_PREFIX = 'sisiwanita-patient-portal-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = '/offline.html';
