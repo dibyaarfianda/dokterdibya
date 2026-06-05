950142ea31e27f9491fccac44b1d08041d083e54
Dibya <nanda.arfianda@gmail.com>
2026-06-05 19:35:55 +0700

    Move booking settings to system menu

--- staff/public/sw.js
+++ staff/public/sw.js
@@ -4,7 +4,7 @@
  * Updated: Real-time friendly for service hours
  */
 
-const STAFF_PWA_VERSION = 'v240'; // Ruang Saya patient activity filter
+const STAFF_PWA_VERSION = 'v241'; // Move Booking Settings to Sistem menu
 const CACHE_NAME = `dokterdibya-staff-${STAFF_PWA_VERSION}`;
 const STATIC_CACHE = `static-${STAFF_PWA_VERSION}`;
 const DYNAMIC_CACHE = `dynamic-${STAFF_PWA_VERSION}`;
