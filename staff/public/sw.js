ba946bef9363015303697ca6d11bd9a2f7d840b9
Dibya <nanda.arfianda@gmail.com>
2026-06-06 10:20:48 +0700

    Restore Supplier menu under Operasional

--- staff/public/sw.js
+++ staff/public/sw.js
@@ -4,7 +4,7 @@
  * Updated: Real-time friendly for service hours
  */
 
-const STAFF_PWA_VERSION = 'v249'; // Sidebar nav overhaul: rename, hide, move, Perhatian Khusus
+const STAFF_PWA_VERSION = 'v250'; // Restore Supplier menu under Operasional
 const CACHE_NAME = `dokterdibya-staff-${STAFF_PWA_VERSION}`;
 const STATIC_CACHE = `static-${STAFF_PWA_VERSION}`;
 const DYNAMIC_CACHE = `dynamic-${STAFF_PWA_VERSION}`;
