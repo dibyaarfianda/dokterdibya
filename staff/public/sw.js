c97845c128ee6f1fac4ff38020eab1a89518e694
Dibya <nanda.arfianda@gmail.com>
2026-06-20 19:30:44 +0700

    Reduce staff PWA mobile font scale

--- staff/public/sw.js
+++ staff/public/sw.js
@@ -4,7 +4,7 @@
  * Updated: Real-time friendly for service hours
  */
 
-const STAFF_PWA_VERSION = 'v261'; // Mobile typography update
+const STAFF_PWA_VERSION = 'v262'; // Mobile typography update
 const CACHE_NAME = `dokterdibya-staff-${STAFF_PWA_VERSION}`;
 const STATIC_CACHE = `static-${STAFF_PWA_VERSION}`;
 const DYNAMIC_CACHE = `dynamic-${STAFF_PWA_VERSION}`;
