3ca7e637643a6302f79074cd6affe6fd82399c14
Dibya <nanda.arfianda@gmail.com>
2026-06-22 17:09:56 +0700

    Bump staff cache version to v269

--- staff/public/sw.js
+++ staff/public/sw.js
@@ -4,7 +4,7 @@
  * Updated: Real-time friendly for service hours
  */
 
-const STAFF_PWA_VERSION = 'v268'; // Sunday Clinic modal selection reset
+const STAFF_PWA_VERSION = 'v269'; // Birth class staff cache refresh
 const CACHE_NAME = `dokterdibya-staff-${STAFF_PWA_VERSION}`;
 const STATIC_CACHE = `static-${STAFF_PWA_VERSION}`;
 const DYNAMIC_CACHE = `dynamic-${STAFF_PWA_VERSION}`;
